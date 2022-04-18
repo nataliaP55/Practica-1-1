a536ba62cd6b7ba1fae05e0791bc9c9fc0dc26dc
yoenotracompu <yoenotracompu@otracompu.com>
2022-04-18 15:10:02 -0300

    actualización de práctica 1-1

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+console.log("soy nati");
+
+document.write("se va");
+
+document.write("<h1> mi primer página </h1>");
+
+document.write("<img src='https://www.educaciontrespuntocero.com/wp-content/uploads/2019/06/homer.gif' alt='gif - img agregada mediante js'>");
